dd4b71b3475dd1bcc3ecb2f79290f7c9c798b08a
briickhub <105386759+briickhub@users.noreply.github.com>
2022-07-13 18:51:25 -0500

    Create funcion1.py
    Creación del archivo funcion1.py, el archivo valida user y password

---
+++ funcion1.py
@@ -0,0 +1,6 @@
+def Validate():
+    user = input("Dame tu usuario: ")
+    password = input ("Dame tu contraseña: ")
+    print("Tu usuario es:", user, "y tu contraseña es:", password)
+
+Validate()
